91f6e5d42ea09e9b117d083fe6edb1376fb5eaf4
Rayan20 <raymondyang76@gmail.com>
2019-07-10 20:45:44 -0500

    Reformatted and added documentation to uno.py

--- uno.py
+++ uno.py
@@ -78,41 +78,54 @@ class Card:
         for i in range(10 - len(str(self.number))):
             spacing += ' '
 
+        # Creating card graphics
         card_str = ' __________ \n|%s%s|\n|          |\n|          |\n|          |\n|          |\n|__________|' % (
         self.number, spacing)
         color_list = ['red', 'green', 'yellow', 'blue']
+
+        # Returns color of card
         if self.color in (color_list):
             return colored(card_str, self.color)
         else:
             return colored(card_str, 'magenta')
 
+    # Allow for card size scaling
     def str_scale(self, width, height=None):
 
 
 class Deck:
     def __init__(self, card_list=None):
+
+        # Initializes empty card list if no cards are appended to the deck
         if card_list is None:
             self.card_list = []
 
+    # Adds card to deck
     def add_card(self, card):
         self.card_list.append(card)
 
+    # Shuffles deck
     def shuffle(self):
         for card in self.card_list:
+
             random_index = random.randint(0, len(self.card_list) - 1)
             temp_card = self.card_list.pop(random_index)
             self.card_list.append(temp_card)
 
+    # Returns a drawn card from deck
     def draw_card(self):
         card = self.card_list.pop(len(self.card_list) - 1)
         return card
 
+    # Returns the index of the card
     def get_card(self, index):
         return self.card_list[index]
 
+    # returns the deck
     def get_card_list(self):
         return self.card_list
 
+    # Returns deck with card visuals
     def __str__(self):
         output = ''
         for card in self.card_list:
